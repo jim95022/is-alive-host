460f351615612dcd9e0877a6ac23340df240560f
Yurii Bykov <jim95022@gmail.com>
2020-11-27 17:52:13 +0300

    added header

--- api/app.py
+++ api/app.py
@@ -5,10 +5,10 @@ def is_alive_host(hostname: str = 'None') -> bool:
     """Проверить, что запрашиваемый хост возвращает http status 100<=x<400."""
 
     if 'http' not in hostname:
-        hostname = 'http://' + hostname
+        hostname = 'https://' + hostname
 
     try:
-        headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+        headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'}
         req = requests.get(hostname, headers=headers, timeout=5)
         response = req.status_code
     except Exception as e:
