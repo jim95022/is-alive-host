a0040903dcba9a0ee647831ef05e1313ff45ede9
Yurii Bykov <jim95022@gmail.com>
2020-11-27 16:59:08 +0300

    some changes

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ def is_alive_host(hostname: str = 'None') -> bool:
         hostname = 'http://' + hostname
 
     try:
-        req = requests.get(hostname, timeout=1)
+        req = requests.get(hostname, timeout=5)
         response = req.status_code
     except:
         response = -1
